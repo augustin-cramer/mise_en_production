8c522b02dc35014697b6a90d4ad05a4b763ff0de
Augustin Cramer <acramer@hotmail.fr>
2024-04-17 16:38:29 +0200

    add generate graphs buttons

--- test_streamlit.py
+++ test_streamlit.py
@@ -10,22 +10,23 @@ option2 = st.selectbox(
     'enter the number of samples',
     (100, 200, 1000))
 
-hist_data = []
-# Add histogram data
-for i in range(option1):
-    hist_data.append(np.random.randn(option2) + 2 * (i - option1/2))
+if st.button('Generate graph'):
+    hist_data = []
+    # Add histogram data
+    for i in range(option1):
+        hist_data.append(np.random.randn(option2) + 2 * (i - option1/2))
 
 
-group_labels = [f'Group {i+1}' for i in range(option1)]
-# Create distplot with custom bin_size
-fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+    group_labels = [f'Group {i+1}' for i in range(option1)]
+    # Create distplot with custom bin_size
+    fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
 
 
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+    tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
 
-with tab1:
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True, theme=None)
-with tab2:
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True, theme="streamlit",)
+    with tab1:
+        # Plot!
+        st.plotly_chart(fig, use_container_width=True, theme=None)
+    with tab2:
+        # Plot!
+        st.plotly_chart(fig, use_container_width=True, theme="streamlit",)
