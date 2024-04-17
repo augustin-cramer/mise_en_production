94b637df977eda87c421c189604b94d0a62fa3fb
Augustin Cramer <acramer@hotmail.fr>
2024-04-17 16:29:31 +0200

    add tabs

--- test_streamlit.py
+++ test_streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import plotly.figure_factory as ff
+import time
 
 option1 = int(st.selectbox(
     'enter the number of groups',
@@ -17,11 +18,17 @@ for i in range(option1):
 
 
 group_labels = [f'Group {i+1}' for i in range(option1)]
-
 # Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
 
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
 
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+
+with tab1:
+    time.sleep(5)
+    # Plot!
+    st.plotly_chart(fig, use_container_width=True, theme=None)
+with tab2:
+    time.sleep(5)
+    # Plot!
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit",)
