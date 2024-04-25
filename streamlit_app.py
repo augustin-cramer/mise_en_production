72adc0f71ecfc7dc7f6b698e05d813f84e7c37b7
Augustin Cramer <acramer@hotmail.fr>
2024-04-25 22:05:07 +0200

    black reformat

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,12 @@ import yaml
 
 s3_config = yaml.safe_load(open("S3_config.yml"))
 ssp_cloud = True
-fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': s3_config['endpoint_url']},key = s3_config["key"], secret = s3_config["secret"], token = s3_config["token"])
+fs = s3fs.S3FileSystem(
+    client_kwargs={"endpoint_url": s3_config["endpoint_url"]},
+    key=s3_config["key"],
+    secret=s3_config["secret"],
+    token=s3_config["token"],
+)
 bucket = "nfarhan/diffusion/mise_en_production"
 
 with_parliament = st.checkbox("With Parliament")
@@ -25,7 +30,6 @@ curves_by_company = st.multiselect(
 )
 
 if st.button("Generate graph"):
-
     tab1, tab2 = st.tabs(["Cosine similarity", "Euclidean distance"])
 
     with tab1:
@@ -34,5 +38,14 @@ if st.button("Generate graph"):
             focus_on_companies = None
         if curves_by_company == []:
             curves_by_company = None
-        fig = choose_projection_cos(axis, sources, focus_on_companies, curves_by_company, with_parliament, ssp_cloud=ssp_cloud, fs=fs, bucket=bucket)
+        fig = choose_projection_cos(
+            axis,
+            sources,
+            focus_on_companies,
+            curves_by_company,
+            with_parliament,
+            ssp_cloud=ssp_cloud,
+            fs=fs,
+            bucket=bucket,
+        )
         st.plotly_chart(fig, use_container_width=True, theme=None)
