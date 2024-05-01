8ceaa0c045c6a924c6d02c698926921412a2b88c
nfarhan <nael.farhan@ensae.fr>
2024-05-01 13:07:30 +0000

    wrong function called for axes variation

--- src/Axes/models.py
+++ src/Axes/models.py
@@ -19,7 +19,7 @@ for i in range(14):  # Assuming 14 years of data, from 2010 to 2023
     # Construct the file path for sentence embeddings
     file_path = f"/without_parliament/sentence_embeddings/sentence_embeddings_201{i}.txt"
     # Load the text data and convert it into a word2vec model for sentences
-    models_s.append(txt_to_model_sentences(file_path, ssp_cloud, fs, bucket))
+    models_s.append(load_txt_model_sentence(file_path, ssp_cloud, fs, bucket))
 
 # Generate word2vec models for words from text files
 models_w = []  # List to store word models
@@ -27,5 +27,5 @@ for i in range(14):  # Loop through the same range for word embeddings
     # Construct the file path for word embeddings
     file_path = f"/without_parliament/embeddings/embeddings_201{i}_WP.txt"
     # Load the text data and convert it into a word2vec model for words
-    models_w.append(txt_to_model_words(file_path, ssp_cloud, fs, bucket))
+    models_w.append(load_txt_model_sentence(file_path, ssp_cloud, fs, bucket))
 
